642f1b430909c08ebc6434af9dc7dfe6bbdba2b1
AntonMhnk <anton.mhnk@yandex.ru>
2024-11-08 16:55:36 +0500

    header edit

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="blue darken-4">
     <div className="nav-wrapper">
-      <a href="https://antonmhnk.github.io/react-movie/" className="brand-logo">React Shop</a>
+      <a href="https://antonmhnk.github.io/react-shop/" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="https://github.com/AntonMhnk/react-shop" target="_blank" rel="noreferrer">Repo</a></li>
+        <li><a href="https://github.com/AntonMhnk/react-shop/" target="_blank" rel="noreferrer">Repo</a></li>
       </ul>
     </div>
   </nav>
